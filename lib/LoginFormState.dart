e72051eed299ead1dfbbfbc668352891e3fc2a7d
Christian Bunk <christian.bunk@sensorberg.com>
2020-05-02 19:10:24 +0200

    Finish Codelab Web App Step 2: Enable sign in progress tracking

--- lib/LoginFormState.dart
+++ lib/LoginFormState.dart
@@ -13,9 +13,29 @@ class LoginFormState extends State<LoginForm> {
     Navigator.of(context).pushNamed('/welcome');
   }
 
+  void _updateFormProgress() {
+    var progress = 0.0;
+    var controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController
+    ];
+
+    for (var controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: () => _updateFormProgress(),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -48,7 +68,7 @@ class LoginFormState extends State<LoginForm> {
           FlatButton(
             color: Colors.blue,
             textColor: Colors.white,
-            onPressed: _showWelcomeScreen,
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: Text('Sign up'),
           ),
         ],
